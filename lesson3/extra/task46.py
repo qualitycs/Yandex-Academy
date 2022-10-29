94d279ec776651a5bb4958596bf727883d092004
Zhangir Begim <slender82@tutanota.com>
2022-10-29 20:06:56 +0600

    Copied some shit from the internet as a start

---
+++ lesson3/extra/task46.py
@@ -0,0 +1,11 @@
+# Python Program to get the first digit of number
+
+# take input
+num = int(input('Enter any Number: '))
+
+# get the first digit
+while (num >= 10):
+    num = num // 10
+
+# printing first digit of number
+print('The first digit of number:', num)
