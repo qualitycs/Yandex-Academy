78e4a8fdbbbbbc92ba0dff990f3a26d9a534f2bd
Zhangir Begim <slender82@tutanota.com>
2022-10-31 14:47:51 +0600

    saving so i don't commit die

--- lesson4/classwork/task52.py
+++ lesson4/classwork/task52.py
@@ -1,7 +1,1 @@
-sentence = input()
-while sentence != 0:
-    print(sentence)
-    sentence = input()
-while sentence == 0:
-    input()
-    sentence = 0
+
---
+++ lesson4/classwork/task54.py
@@ -0,0 +1,9 @@
+total = 0
+price = float(input())
+while price > 0:
+    if price < 1000:
+        total = total + price
+    else:
+        total = total + price * 0.95
+    price = float(input())
+print(total)
