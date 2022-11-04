614b979e2ef42c0808125e2654dd01a7f907defc
Zhangir Begim <slender82@tutanota.com>
2022-11-04 23:56:29 +0600

    Finished binary search insanity

--- lesson4/extra/task64.py
+++ lesson4/extra/task64.py
@@ -1,1 +1,12 @@
-
+low = 1
+high = 1000
+while True:
+    guess = int((low + high) / 2)
+    print(guess)
+    a = input()
+    if a == '=':
+        quit()
+    elif a == '<':
+        high = guess - 1
+    elif a == '>':
+        low = guess + 1
