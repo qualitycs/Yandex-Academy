3b0b15f13772f6e96360b0c67bbcf3e8cbde9ef8
Zhangir Begim <slender82@tutanota.com>
2022-11-06 00:53:04 +0600

    I am in severe pain

--- lesson4/extra/task65.py
+++ lesson4/extra/task65.py
@@ -4,15 +4,19 @@
 # юг = south = (y -=)
 destination_x = input()
 destination_y = input()
-directions = ['y +=', 'x -=', 'y -=', 'x +=']
+directions = ['y+=', 'x-=', 'y-=', 'x+=']
 x, y = 0, 0
 while destination_y != y and destination_x != x:
     command = input()
-    if command != 'вперёд':
+    while command != 'вперёд':
         if command == 'налево':
             directions.append(directions.pop(0))
         if command == 'направо':
             directions.insert(0, directions.pop)
         if command == 'разворот':
             directions.insert(1, directions.pop)
-
+    steps = input()
+    direction = directions[0]
+    exec(direction + steps)
+    # I'm pretty sure that using exec is completely unsafe and probably slow, but the code looks nicer this way lmao
+    print(x, y)
