5219e82c0279a01db8085eb901994855c248f6d2
Zhangir Begim <slender82@tutanota.com>
2022-11-12 22:34:05 +0600

    did stone program

--- lesson5/classwork/task69.py
+++ lesson5/classwork/task69.py
@@ -1,1 +1,7 @@
-
+num = int(input())
+while num > 0:
+    subtract = int(input())
+    if subtract > 3 or subtract < 1 or subtract > num:
+        subtract = 0
+    num = num - subtract
+    print(num)
