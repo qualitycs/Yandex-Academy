8af98db90ed610b3a90681207d1f381fd5651bf1
Zhangir Begim <slender82@tutanota.com>
2022-11-27 18:13:33 +0600

    absolutely based

--- l3_builtInFunctions/extra/t48_fourDigitMinimum.py
+++ l3_builtInFunctions/extra/t48_fourDigitMinimum.py
@@ -1,1 +1,26 @@
-# This is actually fucking impossible lmao
+num = int(input())
+b = num // 1000
+c = num % 1000
+d = c // 100
+e = c % 100
+f = e // 10
+g = e % 10
+if b > d:
+    b, d = d, b
+if d > f:
+    d, f = f, d
+if f > g:
+    f, g = g, f
+if b > d:
+    b, d = d, b
+if d > f:
+    d, f = f, d
+if b > d:
+    b, d = d, b
+if b == 0 and d:
+    b, d = d, b
+elif b == 0 and f:
+    b, f = f, b
+elif b == 0 and g:
+    b, g = g, b
+print(b, d, f, g, sep='')
